c2b47944c008940b8a6b58435f8df18b84852531
YuyaBan <shepherd.1021@gmail.com>
2019-12-15 00:37:46 +0900

    feat: selectbg

--- routes/removebg.js
+++ routes/removebg.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 //const upDir ='/Users/kosuke_matsuoka/Pictures/testfolder/'
-const upDir = '/Users/ban/Pictures/testfolder/studyData/no-bg-back'
+const tmpDir = '/Users/ban/Pictures/testfolder/studyData/tmp/'
+const upDir = '/Users/ban/Pictures/testfolder/studyData/no-bg/'
+const bgDir = '/Users/ban/Pictures/testfolder/studyData/bg/'
 const custumViewUrl ='https://japaneast.api.cognitive.microsoft.com/customvision/v3.0/Prediction/8cff8e09-1e6b-44c9-943a-47b99567af39/classify/iterations/Iteration1/image'
 
 var request = require('request');
@@ -11,13 +13,14 @@ const { PredictionAPIClient } = require("@azure/cognitiveservices-customvision-p
 
 // File  Upload
 var multer = require('multer');
-var upload = multer({ dest: upDir });
+var upload = multer({ dest: tmpDir });
+
+const sharp = require('sharp');
 
 // endpoint1: save the no-bg-image localy
 router.post('/', upload.single('data'), (req, res)=> {
     console.log('in test');
-    const file = req.file
-    const meta = req.body
+    const filename = req.file.filename + '.png'
     // デッバグのため、アップしたファイルの名前を表示する
     console.log(req.file.path);
     console.log('start removebg');
@@ -25,36 +28,85 @@ router.post('/', upload.single('data'), (req, res)=> {
         url: 'https://api.remove.bg/v1.0/removebg',
         formData: {
             //image_file: fs.createReadStream(req.files.path),
-            image_file: fs.createReadStream(upDir + '/'+ req.file.filename),
+            image_file: fs.createReadStream(tmpDir + req.file.filename),
             size: 'auto',
         },
         headers: {
-            'X-Api-Key': ''
+            'X-Api-Key': 'zUDXQupBUaa5MZjiPfdKQRem'
         },
         encoding: null
     }, function(error, response, body) {
     if(error) return console.error('Request failed:', error);
     if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-    fs.writeFileSync(upDir + '/' + "no-bg-test3.png", body);
+    fs.writeFileSync(upDir + filename, body);
     });
+    // sharp(upDir + "no-bg-tmp.png").resize(null,417).toFile(upDir+'save/'+'no-bg-tmp-resize.png', (err, info)=>{
+    //     if(err){
+    //       throw err
+    //     }
+    //     console.log(info)
+    // });
     // アップ完了したら200ステータスを送る
-    res.status(200).json({msg: 'no-bg-test3.png'});
+    res.status(200).json({msg: upDir+'save/'+filename});
 });
 
+// 背景特徴 endpoint
 router.get('/selectbg/:filename', async function (req, res) {
     const predictionKey = "6f04ba8636eb4db9b0e5d5422c5ebc00";
     const predictor = new PredictionAPIClient(predictionKey, custumViewUrl);
-    const testFile = fs.readFileSync(upDir + '/'+ req.params.filename);
+    //const testFile = fs.readFileSync(upDir+'save/'+req.params.filename);
+    const testFile = fs.readFileSync(upDir + req.params.filename);
     const publishIterationName = "Iteration1";
     const projectId = "8cff8e09-1e6b-44c9-943a-47b99567af39";
     const results = await predictor.classifyImage(projectId, publishIterationName, testFile);
 
     console.log("Results:");
     if (results.predictions[0].probability < results.predictions[1].probability) {
-        res.json(`\t ${results.predictions[1].tagName}: ${(results.predictions[1].probability * 100.0).toFixed(2)}%`)
+        res.json({ "tag" : results.predictions[1].tagName });
     } else {
-        res.json(`\t ${results.predictions[0].tagName}: ${(results.predictions[0].probability * 100.0).toFixed(2)}%`)
+        res.json({ "tag" : results.predictions[0].tagName });
     }
+});
+
+// 画像編集 endpoint
+router.get('/editimage/:filename', async function (req, res) {
+    await sharp(upDir + req.params.filename).resize(null,417).toFile(upDir+'save/'+req.params.filename)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+    // sharp(upDir + req.params.filename).grayscale().toFile(upDir + 'save/' + 'output.jpg', (err, info)=>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     console.log(info);
+    // });
+    // await sharp(upDir+req.params.filename).resize(null,417).toFile(upDir+'save/'+'output.png', (err, info)=>{
+    //     if(err){
+    //       throw err
+    //     }
+    //     console.log(info)
+    // });
+    await sharp(bgDir + 'bg1.jpg').composite([{
+        input: upDir+'save/'+req.params.filename
+    }]).toFile(upDir+'save/'+req.params.filename+'-compositite.png')
+    .then(data =>{
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(err);
+    });
+
+    sharp(upDir+'save/'+req.params.filename+'-compositite.png').grayscale().toFile(upDir+'save/'+req.params.filename+'-compositite.png'+'-gray.png')
+    .then(data =>{
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(err);
+    });
+    res.json({status: 200});
 });
 
 module.exports = router;
